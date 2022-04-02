f7ad2b641dd291eccca1262ee07795f1a0d5958e
ChoiYoungHoon <dudgns2865@naver.com>
2022-04-02 17:38:59 +0900

    feat:정보은닉 연

---
+++ Chapter5/src/hiding/BirthDayTest.java
@@ -0,0 +1,46 @@
+package hiding;
+
+class BirthDay {
+	
+	private int day;
+	private int month;
+	private int year;
+	
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		if (month == 2) {
+			if(day < 1 || day>20) {
+				System.out.println("해당 일이 존재하지 않습니다.");
+			}
+		}
+		
+		this.day = day;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+}
+
+public class BirthDayTest {
+	
+	public static void main(String[] args) {
+		
+		BirthDay day = new BirthDay();
+		day.setMonth(2);
+		day.setYear(2018);
+		day.setDay(30);
+		
+	}
+}
